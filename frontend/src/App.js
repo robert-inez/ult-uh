2d00546692d8470a60d1b6c16c224a595e89ff72
Robert Inez <rinez@cnm.edu>
2021-05-31 21:09:00 -0600

    Made progress on ecommerce site

---
+++ frontend/src/App.js
@@ -0,0 +1,51 @@
+import React, {useState, useEffect} from 'react';
+import { commerce } from './lib/commerce';
+import {Products, Navigation, Cart} from './components';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
+const App = () => {
+	const [products, setProducts] = useState([]);
+	const [cart, setCart] = useState({});
+
+	const fetchProducts = async () => {
+		const {data} = await commerce.products.list();
+
+		setProducts(data);
+	};
+
+	const fetchCart = async () => {
+		setCart(await commerce.cart.retrieve())
+	}
+
+	const handleAddToCart = async (productId, quantity) => {
+		const item = await commerce.cart.add(productId, quantity)
+
+		setCart(item.cart)
+	}
+
+	useEffect(() => {
+		fetchProducts();
+		fetchCart()
+	}, []);
+
+	console.log(cart)
+
+	return (
+		<Router>
+			<>
+				<Navigation totalItems={cart.total_items} />
+				<Switch>
+					<Route exact path='/'>
+						<Products products={products} onAddToCart={handleAddToCart} />
+					</Route>
+					<Route exact path='/cart'>
+						<Cart cart={cart} />
+					</Route>
+				</Switch>
+			</>
+		</Router>
+	);
+};
+
+export default App;
---
+++ frontend/src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Navigation } from './Navigation/Navigation';
+export { default as Products } from './products/Products';
+export { default as Cart } from './Cart/Cart';
